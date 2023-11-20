18e245fcfa2a989968e1e9dd928269a28a87c1fc
Leo <deutschleonardo@gmail.com>
2023-11-20 13:53:14 -0600

    Created the employee rest controller endpoint /employees/{employeeId} used to find employee by id, as well as post endpoint to /employees to save a new employee.

--- src/main/java/com/leonardodeutsch/employeerestapi/rest/EmployeeRestController.java
+++ src/main/java/com/leonardodeutsch/employeerestapi/rest/EmployeeRestController.java
@@ -2,9 +2,7 @@ package com.leonardodeutsch.employeerestapi.rest;
 
 import com.leonardodeutsch.employeerestapi.entity.Employee;
 import com.leonardodeutsch.employeerestapi.service.EmployeeService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +19,25 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll() {
         return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if (theEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        return theEmployee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee) {
+
+        theEmployee.setId(0);
+
+        return employeeService.save(theEmployee);
     }
 
 }
