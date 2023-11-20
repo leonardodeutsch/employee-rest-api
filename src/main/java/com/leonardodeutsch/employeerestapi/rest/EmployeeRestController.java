5acb355354ee2b3b8d5a40e673ca34a115f3c538
Leo <deutschleonardo@gmail.com>
2023-11-20 11:49:46 -0600

    Created a rest controller mapped to /api. Added the endpoint /employees which returns a list of all employees from the database.

---
+++ src/main/java/com/leonardodeutsch/employeerestapi/rest/EmployeeRestController.java
@@ -0,0 +1,26 @@
+package com.leonardodeutsch.employeerestapi.rest;
+
+import com.leonardodeutsch.employeerestapi.dao.EmployeeDAO;
+import com.leonardodeutsch.employeerestapi.entity.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+    private EmployeeDAO employeeDAO;
+
+    public EmployeeRestController(EmployeeDAO theEmployeeDAO) {
+        employeeDAO = theEmployeeDAO;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
+    }
+
+}
